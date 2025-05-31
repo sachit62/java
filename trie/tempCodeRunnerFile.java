da1acb39b9e6a6b7399e62544569b680217c596f
Sachit Kapoor <sachitkapoor62@gmail.com>
2025-05-31 21:35:31 +0530

    done 44 now 45 will start

---
+++ trie/tempCodeRunnerFile.java
@@ -0,0 +1,19 @@
+            }
+            curr = curr.children[idx];
+        }
+
+        curr.eow = true;
+    }
+
+    public static boolean search(String key) {
+        Node curr = root;
+        for (int level = 0; level < key.length(); level++) {
+            int idx = key.charAt(level) - 'a';
+            if (curr.children[idx] == null) {
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+
+        return curr.eow == true;
+    }
