66db34061702eaca241a032074cd3ada5b8f8fde
Sachit Kapoor <sachitkapoor62@gmail.com>
2025-12-02 18:49:43 +0530

    recursion

---
+++ recursion/subsequence_sumK2.java
@@ -0,0 +1,32 @@
+// wehn only 1 satisfied condition answer has to be returned
+
+import java.util.*;
+
+public class subsequence_sumK2 {
+
+    public static boolean f(int idx, int[] arr, int n, ArrayList<Integer> a, int sum, int k) {
+        if (idx >= n) {
+            if (sum == k) {
+                System.out.println(a);
+                return true;
+            }
+            return false;
+        }
+        a.add(arr[idx]);
+        sum += arr[idx];
+        if (f(idx + 1, arr, n, a, sum, k) == true)
+            return true;
+        a.remove(a.size() - 1);
+        sum -= arr[idx];
+        if (f(idx + 1, arr, n, a, sum, k) == true)
+            return true;
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 1 };
+        int idx = 0;
+        ArrayList<Integer> a = new ArrayList<>();
+        f(idx, arr, arr.length, a, 0, 2);
+    }
+}
