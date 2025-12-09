6fc6b5bab140b355e5ed0a5062c03e6f72955d39
Sachit Kapoor <sachitkapoor62@gmail.com>
2025-12-09 14:20:57 +0530

    q

--- queue/queueJcf.java
+++ queue/queueJcf.java
@@ -2,16 +2,15 @@ import java.util.*;
 
 public class queueJcf {
     public static void main(String[] args) {
-        // Queue is interface we can implement
-        // it with linkedlist and arrayDeque
         Queue<Integer> q = new LinkedList<>();
-        q.add(1);
-        q.add(2);
-        q.add(3);
-        // 1 2 3
-        while (!q.isEmpty()) {
-            System.out.println(q.peek());
-            q.remove();
-        }
+
+        q.offer(10);
+        q.offer(20);
+        q.offer(30);
+
+        System.out.println(q.peek());   // 10
+        System.out.println(q.poll());   // 10
+        System.out.println(q.remove()); // 20
+        System.out.println(q.isEmpty()); // false
     }
 }
