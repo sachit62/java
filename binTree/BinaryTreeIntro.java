20cfc413c3d7b65905cf9c3eac4be8a121b53ddc
Sachit Kapoor <sachitkapoor62@gmail.com>
2025-12-18 22:57:07 +0530

    greedy

---
+++ binTree/BinaryTreeIntro.java
@@ -0,0 +1,32 @@
+public class BinaryTreeIntro {
+
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+            left = right = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        // Creating nodes
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        /*
+               1
+             /   \
+            2     3
+           / \
+          4   5
+        */
+    }
+}
+
+}
