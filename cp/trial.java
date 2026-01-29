a4ff745856e8e87b2b896a2a09442ebb0ee07497
Sachit Kapoor <sachitkapoor62@gmail.com>
2026-01-29 12:30:31 +0530

    graph

--- cp/trial.java
+++ cp/trial.java
@@ -1,8 +1,13 @@
 import java.util.*;
 
+class Person{
+
+}
 public class trial {
     public static void main(String[] args){
         String s = " hello world ";
         System.out.println(s.length());
+        Person p1 = new Person();
+        System.out.println(p1);
     }
 }
---
+++ graph/tempCodeRunnerFile.java
@@ -0,0 +1,2 @@
+ adj.get(2).add(3);
+        adj.get(3).add(2);
